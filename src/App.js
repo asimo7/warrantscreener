13f4e41da4fd507c5eca39c7a7124cfc0e2effc0
asimo7 <117193226+asimo7@users.noreply.github.com>
2024-11-26 13:28:02 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import "./App.css";
 
-const socket = io("https://asimo7.github.io");
+const socket = io("http://localhost:5000");
 
 function App() {
   const [warrants, setWarrants] = useState([]);
